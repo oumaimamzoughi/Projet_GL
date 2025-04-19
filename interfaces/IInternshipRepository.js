b04d71d50e5b17846ddfcb319001d6faae15f95a
Oumaima Mzoughi <oumaymaamzoughi@gmail.com>
2025-04-19 17:08:07 +0100

    interfaces for InternshipRepository et IuserRepository

---
+++ interfaces/IInternshipRepository.js
@@ -0,0 +1,9 @@
+module.exports = class IInternshipRepository {
+    async create(data) { throw new Error("Not implemented"); }
+    async findById(id) { throw new Error("Not implemented"); }
+    async findAll(filter) { throw new Error("Not implemented"); }
+    async update(id, data) { throw new Error("Not implemented"); }
+    async delete(id) { throw new Error("Not implemented"); }
+    async findByType(type) { throw new Error("Not implemented"); }
+    async assignTeacher(internshipId, teacherId) { throw new Error("Not implemented"); }
+  };
---
+++ interfaces/IUserRepository.js
@@ -0,0 +1,6 @@
+module.exports = class IUserRepository {
+    async findById(id) { throw new Error("Not implemented"); }
+    async save(user) { throw new Error("Not implemented"); }
+    async findByRole(role) { throw new Error("Not implemented"); }
+    async findTeachers() { throw new Error("Not implemented"); }
+  };
